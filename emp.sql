228681539af78b9d7bbd64d6a5974bbc552c53c8
Jayesh0408 <63913514+Jayesh0408@users.noreply.github.com>
2021-08-03 16:04:58 +0530

    Add files via upload

---
+++ emp.sql
@@ -0,0 +1,40 @@
+/*
+SQLyog Community v11.1 (64 bit)
+MySQL - 5.1.73-community : Database - demo
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`demo` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `demo`;
+
+/*Table structure for table `employee` */
+
+DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `employee` (
+  `firstName` varchar(500) DEFAULT NULL,
+  `lastName` varchar(500) DEFAULT NULL,
+  `employeeId` int(100) NOT NULL,
+  `email` varchar(500) DEFAULT NULL,
+  `dateOfJoining` varchar(100) DEFAULT NULL,
+  `department` varchar(100) NOT NULL,
+  PRIMARY KEY (`employeeId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `employee` */
+
+insert  into `employee`(`firstName`,`lastName`,`employeeId`,`email`,`dateOfJoining`,`department`) values ('parth','shaha',101,'p@g.com','12/12/2012','Chemistry'),('kunal','shaha',102,'k@m.com','01/01/2012','Computer');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
